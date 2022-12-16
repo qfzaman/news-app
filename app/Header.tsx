e4d5fe0512fdb203b58abf2dada0bb485c304724
Quazi Fahmiduzzaman <quazi.fahmiduzzaman@gmail.com>
2022-12-17 02:57:04 +0600

    SearchBox

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,6 +1,7 @@
 import { Bars3Icon } from '@heroicons/react/24/solid'
 import Link from 'next/link'
 import NavLinks from './NavLinks'
+import SearchBox from './SearchBox'
 
 function Header() {
     return (
@@ -20,7 +21,7 @@ function Header() {
 
             <NavLinks />
 
-            {/* SearchBox */}
+            <SearchBox />
         </header>
     )
 }
