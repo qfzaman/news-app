a8d6d0ca9b12c822cf544ea3559e31eab2099216
Quazi Fahmiduzzaman <quazi.fahmiduzzaman@gmail.com>
2022-12-17 02:32:43 +0600

    NavLinks

---
+++ app/NavLink.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+type Props = {
+    category: string
+    isActive: boolean
+}
+
+function NavLink({ category, isActive }: Props) {
+    return (
+        <Link href={`/news/${category}`} className={`navLink ${isActive && 'underline decoration-red-500 underline-offset-4 font-bold text-lg'}`}>
+            {category}
+        </Link>
+    )
+}
+
+export default NavLink
