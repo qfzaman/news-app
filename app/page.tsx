ba7fdee9cd9088d634e0a1b18a4ceb5a0e8d7aad
Quazi Fahmiduzzaman <quazi.fahmiduzzaman@gmail.com>
2022-12-17 06:41:00 +0600

    Integrated Articles

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { categories } from '../constants'
+import fetchNews from '../lib/fetchNews'
+import NewsList from './NewsList'
+import response from '../response.json'
 
-function Homepagepage() {
-    return <div>HomePage</div>
+async function Homepagepage() {
+    const news: NewsResponse = await fetchNews(categories.join(','))
+
+    return (
+        <div>
+            <NewsList news={news} />
+        </div>
+    )
 }
 
 export default Homepagepage
