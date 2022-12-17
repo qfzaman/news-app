4c04bc3720c870f69dcb4abff2e4ba7cf4aad848
Quazi Fahmiduzzaman <quazi.fahmiduzzaman@gmail.com>
2022-12-17 16:32:02 +0600

    added portfolio link

--- app/Header.tsx
+++ app/Header.tsx
@@ -8,7 +8,9 @@ function Header() {
     return (
         <header>
             <div className="grid grid-cols-3 p-10 items-center">
-                <Bars3Icon className="h-8 w-8 cursor-pointer" href="/" />
+                <Link href="/">
+                    <Bars3Icon className="h-8 w-8 cursor-pointer" />
+                </Link>
                 <Link href="/" prefetch={false}>
                     <h1 className="font-serif text-4xl text-center">
                         <span className="underline decoration-6 decoration-red-500">Live</span> News
@@ -16,7 +18,11 @@ function Header() {
                 </Link>
                 <div className="flex items-center justify-end space-x-2">
                     <DarkModeButton />
-                    <button className="hidden md:inline bg-slate-900 text-white px-4 lg:px-8 py-2 lg:py-4 rounded-full dark:bg-slate-800">Portfolio</button>
+                    <Link legacyBehavior href="https://www.qfzaman.com" passHref>
+                        <a target="_blank">
+                            <button className="hidden md:inline bg-slate-900 text-white px-4 lg:px-8 py-2 lg:py-4 rounded-full dark:bg-slate-800">Portfolio</button>
+                        </a>
+                    </Link>
                 </div>
             </div>
 
